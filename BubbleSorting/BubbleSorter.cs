1a767d478566843e0d9d7afbbf00f6a6150f085d
PavelZakharenko <pavel.zakharenko22@gmail.com>
2015-10-15 18:19:38 +0300

    Day1

---
+++ BubbleSorting/BubbleSorter.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSorting
+{
+    public class BubbleSorter
+    {
+        delegate int ToS(int[] arr);
+        delegate bool OrS(int a, int b);
+        public static void Sorting(int[][] arr, Order or, TypeOfSort ts)
+        {
+            int capacity = PreSorting(arr);
+            ToS O;
+            OrS op;
+            switch (ts)
+            {
+                case TypeOfSort.Min:
+                    O = new ToS(MinElem);
+                    break;
+                case TypeOfSort.Max:
+                    O = new ToS(MaxElem);
+                    break;
+                case TypeOfSort.Summ:
+                    O = new ToS(SummArr);
+                    break;
+                default:
+                    O = new ToS(MinElem);
+                    break;
+            }
+            switch (or)
+            {
+                case Order.Inverse:
+                    op = new OrS(CompR);
+                    break;
+                case Order.Straight:
+                    op = new OrS(CompS);
+                    break;
+
+                default:
+                    op = new OrS(CompS);
+                    break;
+            }
+            for (int i = 0; i < capacity; i++)
+            {
+                for (int j = capacity - 1; j > i; j--)
+                {
+                    if (op(O(arr[j]), O(arr[j - 1])))
+                    {
+
+                        int[] ar = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = ar;
+                    }
+                }
+            }
+
+        }
+
+        static int PreSorting(int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[j - 1] == null && arr[j] != null)
+                    {
+                        int[] ar = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = ar;
+                    }
+                }
+            }
+            int C = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != null) C++;
+            }
+            return C;
+        }
+        static int MaxElem(int[] arr)
+        {
+            int Max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Max < arr[i]) Max = arr[i];
+            }
+            return Max;
+        }
+        static int MinElem(int[] arr)
+        {
+            int Min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Min < arr[i]) Min = arr[i];
+            }
+            return Min;
+        }
+        static int SummArr(int[] arr)
+        {
+            int Summ = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Summ += arr[i];
+            }
+            return Summ;
+        }
+        static bool CompR(int a, int b)
+        {
+            return a > b;
+        }
+        static bool CompS(int a, int b)
+        {
+            return a < b;
+        }
+
+    }
+    public enum Order
+    {
+        Inverse,
+        Straight
+    }
+    public enum TypeOfSort
+    {
+        Min,
+        Max,
+        Summ
+    }
+}
---
+++ NewtonMethod/Newton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonMethod
+{
+    public class Newton
+    {
+        public static double Root(double Value, int n, double eps)
+        {
+            double xn, xo;
+            xn = 0;
+            xo = 1;
+            double k = 1.0 / n;
+            while (Math.Abs(xo - xn) > eps)
+            {
+
+                xn = k * ((n - 1) * xo + (Value / Pow(xo, n - 1)));
+                xo = xn;
+            }
+
+            return xn;
+
+        }
+        static double Pow(double Value, int n)
+        {
+            if (n == 0) return 1;
+            else
+            {
+                double numb = Value;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    numb = numb * Value;
+                }
+                return numb;
+            }
+        }
+    }
+}
