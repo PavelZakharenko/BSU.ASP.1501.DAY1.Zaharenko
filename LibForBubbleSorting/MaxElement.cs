7df9d8f4aab033801df2126be877e86e932f3a46
PavelZakharenko <pavel.zakharenko22@gmail.com>
2015-10-26 15:56:39 +0300

    libforsorting

---
+++ LibForBubbleSorting/MaxElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BubbleSorting;
+
+namespace LibForBubbleSorting
+{
+    public class MAxElement :IComparabe
+    {
+        bool IsRightOrder=true;
+        public MAxElement(bool isRightOrder)
+        {
+            IsRightOrder = isRightOrder;
+        }
+        public bool Compare(int[] current,int[] prev)
+        {
+            if (IsRightOrder)
+            {
+                if (MaxElem(prev) < MaxElem(current))
+                    return true;
+                return false;
+            }
+            if (MaxElem(prev) > MaxElem(current))
+                return true;
+            return false;
+
+        }
+        static int MaxElem(int[] arr)
+        {
+            int Max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Max < arr[i]) Max = arr[i];
+            }
+            return Max;
+        }
+    }
+}
---
+++ LibForBubbleSorting/SumElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BubbleSorting;
+
+namespace LibForBubbleSorting
+{
+  public  class SumElements: IComparabe
+    {
+        bool IsRightOrder;
+        public SumElements(bool isRightOrder)
+        {
+            IsRightOrder = isRightOrder;
+        }
+        public bool Compare(int[] current, int[] prev)
+        {
+            if (IsRightOrder)
+            {
+                if (SummArr(prev) < SummArr(current))
+                    return true;
+                return false;
+            }
+            if (SummArr(prev) > SummArr(current))
+                return true;
+            return false;
+
+        }
+        static int SummArr(int[] arr)
+        {
+            int Summ = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Summ += arr[i];
+            }
+            return Summ;
+        }
+    }
+}
